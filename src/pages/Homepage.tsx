3c76d9997791f9b2e19717d4aeff868ce5d70df6
Strozier <strozierrashida@vineyardcolumbus.org>
2024-12-16 11:19:40 -0500

    added image to homepage, added routing, ran a build for tailwind, added pages for nav, added navbar

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,17 @@
+import DanielleImage from '../assets/danielle-main-image.jpg';
+
 const Homepage = () => {
   return (
     <>
       <div className="mini-spacer"></div>
       <div className="centered">Mama Suz Crochet</div>
+      <div className="centered">
+      <img 
+        src={DanielleImage} 
+        alt="Danielle" 
+        style={{ maxWidth: '100%', height: '25rem' }} 
+      />
+      </div>
     </>
   );
 };
